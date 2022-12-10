cdf53f64f16bf9f93bbbbc0c1ed43de329fc182e
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-12-10 14:53:36 +0200

    prepared folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div className="text-4xl">App</div>;
+  return (
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex text-white">
+      App
+    </div>
+  );
 };
 
 export default App;
