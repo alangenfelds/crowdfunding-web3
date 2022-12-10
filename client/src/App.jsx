6533c5a2b43cdc320c18000f73b8c5256e8da1af
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-12-10 17:59:50 +0200

    Sidebar & Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Home, CreateCampaign, CampaignDetails, Profile } from './pages';
+
+import { Navbar, Sidebar } from './components';
 
 const App = () => {
   return (
-    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex text-white">
-      App
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex text-white font-epilogue">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
   );
 };
